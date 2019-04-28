347c1322ed3cbffc35bd30443895fb8b1cf31b5f
G-T <guillaume.tripier1@gmail.com>
2019-04-28 20:32:44 +0200

    Fix sign problem

--- src/api/auth/middlewares/signin.js
+++ src/api/auth/middlewares/signin.js
@@ -1,12 +1,14 @@
 import jwt from 'jsonwebtoken';
-import config from 'config';
+// import config from 'config';
 import bcrypt from 'bcrypt';
 import User from '../../../models/index';
+
+const JWT_SECRET = process.env.jwt_secret;
 
 export default function (req, res, next) {
   User.findOne({ where: { email: req.body.email } }).then((user, err) => {
     if (err || !user) { next({ status: 400, message: 'Wrong email or password' }); } else if (bcrypt.hashSync(req.body.password, user.salt) === user.password) {
-      const token = jwt.sign({ userUUID: user.uuid }, config.get('jwt_secret'));
+      const token = jwt.sign({ userUUID: user.uuid }, JWT_SECRET);
       res.send({ token });
     } else { next({ message: 'Password is not correct' }); }
   });
