9ef741e2653b1b5f6da204b55b07474daf77bcf1
G-T <guillaume.tripier1@gmail.com>
2019-06-05 20:30:35 +0200

    full Region feeder

--- src/feeder.js
+++ src/feeder.js
@@ -3,10 +3,12 @@ import bcrypt from 'bcrypt';
 import userService from './modules/user/services';
 import cellarService from './modules/cellar/services';
 import wineTypeService from './modules/wineType/services';
+import regionService from './modules/region/services';
 
 import User from './models/user/user';
 import Cellar from './models/cellar/cellar';
 import WineType from './models/wineType/wineType';
+import Region from './models/region/region';
 
 const userEmail = 'e@mail.com';
 const userPassword = 'password';
@@ -54,8 +56,69 @@ const createWineTypes = async () => {
   });
 };
 
+const createRegion = async () => {
+  await Region.sync({ force: true });
+
+  await regionService.create({
+    name: 'Alsace',
+  });
+
+  await regionService.create({
+    name: 'Beaujolais',
+  });
+
+  await regionService.create({
+    name: 'Bordeaux',
+  });
+
+  await regionService.create({
+    name: 'Bourgogne',
+  });
+
+  await regionService.create({
+    name: 'Champagne',
+  });
+
+  await regionService.create({
+    name: 'Corse',
+  });
+
+  await regionService.create({
+    name: 'Jura',
+  });
+
+  await regionService.create({
+    name: 'Languedoc-Roussillon',
+  });
+
+  await regionService.create({
+    name: 'Loire',
+  });
+
+  await regionService.create({
+    name: 'Provence',
+  });
+
+  await regionService.create({
+    name: 'Valée du rône',
+  });
+
+  await regionService.create({
+    name: 'Savoie-Bugey',
+  });
+
+  await regionService.create({
+    name: 'Sud-ouest',
+  });
+
+  await regionService.create({
+    name: 'Moselle',
+  });
+};
+
 export default async () => {
   await createUsers();
   await createCellars();
   await createWineTypes();
+  await createRegion();
 };
