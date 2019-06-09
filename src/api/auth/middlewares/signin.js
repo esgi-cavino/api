780da2d0385ea9886be23941b6bfb2f4014396a9
G-T <guillaume.tripier1@gmail.com>
2019-06-09 20:01:52 +0200

    Set JWT duration option

--- src/api/auth/middlewares/signin.js
+++ src/api/auth/middlewares/signin.js
@@ -4,11 +4,11 @@ import bcrypt from 'bcrypt';
 import { User } from '../../../models/index';
 
 const JWT_SECRET = process.env.jwt_secret;
-
+const JWT_VALID_DURATION = process.env.jwt_validDuration;
 export default function (req, res, next) {
   User.findOne({ where: { email: req.body.email } }).then((user, err) => {
     if (err || !user) { next({ status: 400, message: 'Wrong email or password' }); } else if (bcrypt.hashSync(req.body.password, user.salt) === user.password) {
-      const token = jwt.sign({ userUUID: user.uuid }, JWT_SECRET);
+      const token = jwt.sign({ userUUID: user.uuid }, JWT_SECRET, { expiresIn: JWT_VALID_DURATION });
       res.send({ token });
     } else { next({ message: 'Password is not correct' }); }
   });
