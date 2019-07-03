ab89fd0230de7757de7742cc823440bbb38805ef
G-T <guillaume.tripier1@gmail.com>
2019-07-03 18:39:04 +0200

    Refactoring WineType CRUD routes

--- src/api/wineType/index.js
+++ src/api/wineType/index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import create from './middlewares/create';
-import getAll from './middlewares/getAll';
-import findOne from './middlewares/findOne';
-import deleteOne from './middlewares/deleteOne';
-import updateOne from './middlewares/updateOne';
+import create from '../middleware/CRUD/create';
+import getAll from '../middleware/CRUD/getAll';
+import findOne from '../middleware/CRUD/findOne';
+import deleteOne from '../middleware/CRUD/deleteOne';
+import updateOne from '../middleware/CRUD/updateOne';
+import { WineType } from '../../models';
 
 const wineTypeRouter = Router();
 const wineTypeAuthRouter = Router();
@@ -12,14 +13,14 @@ const wineTypeAdminRouter = Router();
 
 const table = '/wineType';
 
-wineTypeAuthRouter.get(table, getAll);
+wineTypeAuthRouter.get(table, getAll.bind(null, WineType));
 
-wineTypeAuthRouter.get(`${table}/:id`, findOne);
+wineTypeAuthRouter.get(`${table}/:id`, findOne.bind(null, WineType));
 
-wineTypeAdminRouter.patch(`${table}/:id`, updateOne);
+wineTypeAdminRouter.patch(`${table}/:id`, updateOne.bind(null, WineType));
 
-wineTypeAuthRouter.post(`${table}`, create);
+wineTypeAuthRouter.post(`${table}`, create.bind(null, WineType));
 
-wineTypeAdminRouter.delete(`${table}/:id`, deleteOne);
+wineTypeAdminRouter.delete(`${table}/:id`, deleteOne.bind(null, WineType));
 
 export { wineTypeRouter, wineTypeAuthRouter, wineTypeAdminRouter };
