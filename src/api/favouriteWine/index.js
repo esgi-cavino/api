bb3101261d33ec1b14ca3ec0af4ee9077f0881fc
G-T <guillaume.tripier1@gmail.com>
2019-07-03 18:56:52 +0200

    Refactoring FavouriteWine CRUD routes

--- src/api/favouriteWine/index.js
+++ src/api/favouriteWine/index.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
-import create from './middlewares/create';
-import getAll from './middlewares/getAll';
-import findOne from './middlewares/findOne';
-import deleteOne from './middlewares/deleteOne';
-import updateOne from './middlewares/updateOne';
+import create from '../middleware/CRUD/create';
+import getAll from '../middleware/CRUD/getAll';
+import findOne from '../middleware/CRUD/findOne';
+import deleteOne from '../middleware/CRUD/deleteOne';
+import updateOne from '../middleware/CRUD/updateOne';
 import deleteByIdAndUserUUID from './middlewares/deleteByIdAndUserUUID';
+import { FavouriteWine } from '../../models';
 
 const favouriteWineRouter = Router();
 const favouriteWineAuthRouter = Router();
@@ -13,16 +14,16 @@ const favouriteWineAdminRouter = Router();
 
 const table = '/favouriteWine';
 
-favouriteWineAuthRouter.get(table, getAll);
+favouriteWineAuthRouter.get(table, getAll.bind(null, FavouriteWine));
 
-favouriteWineAuthRouter.get(`${table}/:id`, findOne);
+favouriteWineAuthRouter.get(`${table}/:id`, findOne.bind(null, FavouriteWine));
 
-favouriteWineAuthRouter.patch(`${table}/:id`, updateOne);
+favouriteWineAuthRouter.patch(`${table}/:id`, updateOne.bind(null, FavouriteWine));
 
-favouriteWineAuthRouter.post(`${table}`, create);
+favouriteWineAuthRouter.post(`${table}`, create.bind(null, FavouriteWine));
 
 favouriteWineAuthRouter.delete(`${table}/:id/:userUUID`, deleteByIdAndUserUUID);
 
-favouriteWineAdminRouter.delete(`${table}/:id`, deleteOne);
+favouriteWineAdminRouter.delete(`${table}/:id`, deleteOne.bind(null, FavouriteWine));
 
 export { favouriteWineRouter, favouriteWineAuthRouter, favouriteWineAdminRouter };
