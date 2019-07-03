994427bf08000fc0b2bb354ea399044f5d7eab18
G-T <guillaume.tripier1@gmail.com>
2019-07-03 18:46:37 +0200

    Refactoring Domain CRUD routes

--- src/api/domain/index.js
+++ src/api/domain/index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import create from './middlewares/create';
-import getAll from './middlewares/getAll';
-import findOne from './middlewares/findOne';
-import deleteOne from './middlewares/deleteOne';
-import updateOne from './middlewares/updateOne';
+import create from '../middleware/CRUD/create';
+import getAll from '../middleware/CRUD/getAll';
+import findOne from '../middleware/CRUD/findOne';
+import deleteOne from '../middleware/CRUD/deleteOne';
+import updateOne from '../middleware/CRUD/updateOne';
+import { Domain } from '../../models';
 
 const domainRouter = Router();
 const domainAuthRouter = Router();
@@ -12,14 +13,14 @@ const domainAdminRouter = Router();
 
 const table = '/domain';
 
-domainAuthRouter.get(table, getAll);
+domainAuthRouter.get(table, getAll.bind(null, Domain));
 
-domainAuthRouter.get(`${table}/:id`, findOne);
+domainAuthRouter.get(`${table}/:id`, findOne.bind(null, Domain));
 
-domainAdminRouter.patch(`${table}/:id`, updateOne);
+domainAdminRouter.patch(`${table}/:id`, updateOne.bind(null, Domain));
 
-domainAuthRouter.post(`${table}`, create);
+domainAuthRouter.post(`${table}`, create.bind(null, Domain));
 
-domainAdminRouter.delete(`${table}/:id`, deleteOne);
+domainAdminRouter.delete(`${table}/:id`, deleteOne.bind(null, Domain));
 
 export { domainRouter, domainAuthRouter, domainAdminRouter };
