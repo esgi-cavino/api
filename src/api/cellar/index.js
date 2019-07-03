f2b4a2c0fdb33f6da2e98db3a5699b8fae20c981
G-T <guillaume.tripier1@gmail.com>
2019-07-03 19:05:56 +0200

    Refactoring Cellar CRUD services and routes

--- src/api/cellar/index.js
+++ src/api/cellar/index.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
-import create from './middlewares/create';
-import getAll from './middlewares/getAll';
-import findOne from './middlewares/findOne';
-import deleteOne from './middlewares/deleteOne';
-import updateOne from './middlewares/updateOne';
+import create from '../middleware/CRUD/create';
+import getAll from '../middleware/CRUD/getAll';
+import findOne from '../middleware/CRUD/findOne';
+import deleteOne from '../middleware/CRUD/deleteOne';
+import updateOne from '../middleware/CRUD/updateOne';
 import deleteByIdAndUserUUID from './middlewares/deleteByIdAndUserUUID';
+import { Cellar } from '../../models';
 
 const cellarRouter = Router();
 const cellarAuthRouter = Router();
@@ -13,16 +14,16 @@ const cellarAdminRouter = Router();
 
 const table = '/cellar';
 
-cellarAdminRouter.get(table, getAll);
+cellarAdminRouter.get(table, getAll.bind(null, Cellar));
 
-cellarAuthRouter.get(`${table}/:id`, findOne);
+cellarAuthRouter.get(`${table}/:id`, findOne.bind(null, Cellar));
 
-cellarAuthRouter.patch(`${table}/:id`, updateOne);
+cellarAuthRouter.patch(`${table}/:id`, updateOne.bind(null, Cellar));
 
-cellarAuthRouter.post(`${table}`, create);
+cellarAuthRouter.post(`${table}`, create.bind(null, Cellar));
 
 cellarAuthRouter.delete(`${table}/:id/:userUUID`, deleteByIdAndUserUUID);
 
-cellarAdminRouter.delete(`${table}/:id`, deleteOne);
+cellarAdminRouter.delete(`${table}/:id`, deleteOne.bind(null, Cellar));
 
 export { cellarRouter, cellarAuthRouter, cellarAdminRouter };
--- src/api/cellar/middlewares/deleteOne.js
+++
@@ -1,8 +0,0 @@
-import cellarServices from '../../../modules/cellar/services';
-
-export default function (req, res, next) {
-  return cellarServices
-    .deleteOne(req.params.id)
-    .then(response => res.sendStatus(response))
-    .catch(err => next(err));
-}
