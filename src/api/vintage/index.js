9d2f04ae146f5d52d9f39b270ff98def7aa85b34
G-T <guillaume.tripier1@gmail.com>
2019-07-03 18:35:55 +0200

    Refactoring Vintage CRUD routes

--- src/api/vintage/index.js
+++ src/api/vintage/index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import create from './middlewares/create';
-import getAll from './middlewares/getAll';
-import findOne from './middlewares/findOne';
-import deleteOne from './middlewares/deleteOne';
-import updateOne from './middlewares/updateOne';
+import create from '../middleware/CRUD/create';
+import getAll from '../middleware/CRUD/getAll';
+import findOne from '../middleware/CRUD/findOne';
+import deleteOne from '../middleware/CRUD/deleteOne';
+import updateOne from '../middleware/CRUD/updateOne';
+import { Vintage } from '../../models';
 
 const vintageRouter = Router();
 const vintageAuthRouter = Router();
@@ -12,14 +13,14 @@ const vintageAdminRouter = Router();
 
 const table = '/vintage';
 
-vintageAuthRouter.get(table, getAll);
+vintageAuthRouter.get(table, getAll.bind(null, Vintage));
 
-vintageAuthRouter.get(`${table}/:id`, findOne);
+vintageAuthRouter.get(`${table}/:id`, findOne.bind(null, Vintage));
 
-vintageAdminRouter.patch(`${table}/:id`, updateOne);
+vintageAdminRouter.patch(`${table}/:id`, updateOne.bind(null, Vintage));
 
-vintageAuthRouter.post(`${table}`, create);
+vintageAuthRouter.post(`${table}`, create.bind(null, Vintage));
 
-vintageAdminRouter.delete(`${table}/:id`, deleteOne);
+vintageAdminRouter.delete(`${table}/:id`, deleteOne.bind(null, Vintage));
 
 export { vintageRouter, vintageAuthRouter, vintageAdminRouter };
