647da0ecaef324bc5a8bd987be98e7cefb6ad7a2
G-T <guillaume.tripier1@gmail.com>
2019-07-03 21:46:36 +0200

    Refactoring PositionInCellar CRUD routes

--- src/api/positionInCellar/index.js
+++ src/api/positionInCellar/index.js
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 
-import create from './middlewares/create';
-import getAll from './middlewares/getAll';
-import findOne from './middlewares/findOne';
-import deleteOne from './middlewares/deleteOne';
-import updateOne from './middlewares/updateOne';
+import create from '../middleware/CRUD/create';
+import getAll from '../middleware/CRUD/getAll';
+import findOne from '../middleware/CRUD/findOne';
+import deleteOne from '../middleware/CRUD/deleteOne';
+import updateOne from '../middleware/CRUD/updateOne';
 import getAllByCellarId from './middlewares/getAllByCellarId';
 import getAllByCellarAndBottleId from './middlewares/getAllByCellarAndBottleId';
 import findByCellarIdAndPosition from './middlewares/findByCellarIdAndPosition';
 import deleteOneByCellarIdAndPosition from './middlewares/deleteOneByCellarIdAndPosition';
+import { PositionInCellar } from '../../models';
 
 const positionInCellarRouter = Router();
 const positionInCellarAuthRouter = Router();
@@ -16,7 +17,7 @@ const positionInCellarAdminRouter = Router();
 
 const table = '/positionInCellar';
 
-positionInCellarAdminRouter.get(table, getAll);
+positionInCellarAdminRouter.get(table, getAll.bind(null, PositionInCellar));
 
 positionInCellarAuthRouter.get(`${table}/byCellarId/:cellarId`, getAllByCellarId);
 
@@ -24,13 +25,13 @@ positionInCellarAuthRouter.get(`${table}/byCellarAndBottleId/:cellarId/:bottleId
 
 positionInCellarAuthRouter.get(`${table}/byCellarIdAndPosition/:cellarId/:positionX/:positionY`, findByCellarIdAndPosition);
 
-positionInCellarAuthRouter.get(`${table}/:id`, findOne);
+positionInCellarAuthRouter.get(`${table}/:id`, findOne.bind(null, PositionInCellar));
 
-positionInCellarAuthRouter.patch(`${table}/:id`, updateOne);
+positionInCellarAuthRouter.patch(`${table}/:id`, updateOne.bind(null, PositionInCellar));
 
-positionInCellarAuthRouter.post(`${table}`, create);
+positionInCellarAuthRouter.post(`${table}`, create.bind(null, PositionInCellar));
 
-positionInCellarAdminRouter.delete(`${table}/:id`, deleteOne);
+positionInCellarAdminRouter.delete(`${table}/:id`, deleteOne.bind(null, PositionInCellar));
 
 positionInCellarAuthRouter.delete(`${table}/byCellarIdAndPosition/:cellarId/:positionX/:positionY`, deleteOneByCellarIdAndPosition);
 
