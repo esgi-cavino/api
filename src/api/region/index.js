38a5feeffa8da4867244560c6fb6ad02a7e6b577
G-T <guillaume.tripier1@gmail.com>
2019-07-03 18:43:46 +0200

    Refactoring Region CRUD routes

--- src/api/region/index.js
+++ src/api/region/index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import create from './middlewares/create';
-import getAll from './middlewares/getAll';
-import findOne from './middlewares/findOne';
-import deleteOne from './middlewares/deleteOne';
-import updateOne from './middlewares/updateOne';
+import create from '../middleware/CRUD/create';
+import getAll from '../middleware/CRUD/getAll';
+import findOne from '../middleware/CRUD/findOne';
+import deleteOne from '../middleware/CRUD/deleteOne';
+import updateOne from '../middleware/CRUD/updateOne';
+import { Region } from '../../models';
 
 const regionRouter = Router();
 const regionAuthRouter = Router();
@@ -12,14 +13,14 @@ const regionAdminRouter = Router();
 
 const table = '/region';
 
-regionAuthRouter.get(table, getAll);
+regionAuthRouter.get(table, getAll.bind(null, Region));
 
-regionAuthRouter.get(`${table}/:id`, findOne);
+regionAuthRouter.get(`${table}/:id`, findOne.bind(null, Region));
 
-regionAdminRouter.patch(`${table}/:id`, updateOne);
+regionAdminRouter.patch(`${table}/:id`, updateOne.bind(null, Region));
 
-regionAuthRouter.post(`${table}`, create);
+regionAuthRouter.post(`${table}`, create.bind(null, Region));
 
-regionAdminRouter.delete(`${table}/:id`, deleteOne);
+regionAdminRouter.delete(`${table}/:id`, deleteOne.bind(null, Region));
 
 export { regionRouter, regionAuthRouter, regionAdminRouter };
